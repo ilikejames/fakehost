6a12c091cc604a9372efcd8861479d4dde66d019
ilikejames <james.mountain@gmail.com>
2022-10-23 12:05:02 +0100

    feat(exchange): add event listeners for disconnect and connect

--- packages/exchange/src/host/BaseFakeHost.ts
+++ packages/exchange/src/host/BaseFakeHost.ts
@@ -1,65 +1,97 @@
 import { ProtocolHandler } from '../ProtocolHandler'
 
 export interface HostOptions {
-  name?: string
-  debug?: boolean
+    name?: string
+    debug?: boolean
 }
 export interface Connection {
-  close: () => void
-  readonly id: string
-  write: (message: string) => void
-  isClosed?: boolean
-  query?: Record<string, string | string[] | undefined>
+    close: () => void
+    readonly id: string
+    write: (message: string) => void
+    isClosed?: boolean
+    query?: Record<string, string | string[] | undefined>
 }
+
+type EventType = 'connection' | 'disconnect'
+
+type EventHandler = (r: { event: EventType; id: string }) => void
 
 export interface FakeHost {
-  readonly url: Promise<string>
-  dispose: () => Promise<void>
-  disconnect: () => void
-  getConnections: () => Connection[]
-  start: (port?: number) => void
-  refuseNewConnections: boolean
+    readonly url: Promise<string>
+    dispose: () => Promise<void>
+    disconnect: () => void
+    getConnections: () => Connection[]
+    start: (port?: number) => void
+    refuseNewConnections: boolean
+    addEventListener: (event: EventType, handler: EventHandler) => void
+    removeEventListener: (event: EventType, handler: EventHandler) => void
 }
 
-export abstract class BaseFakeHost implements FakeHost {
-  private readonly _connections = new Array<Connection>()
+export abstract class BaseFakeHost<I, O> implements FakeHost {
+    private readonly _connections = new Array<Connection>()
+    private eventHandlers = new Array<{ event: EventType; handler: EventHandler }>()
 
-  constructor (public readonly protocolHandler: ProtocolHandler<unknown, unknown>) {}
+    constructor(public readonly protocolHandler: ProtocolHandler<I, O>) {}
 
-  protected onConnection (connection: Connection) {
-    this._connections.unshift({
-      ...connection,
-      isClosed: false
-    })
-    if (this.protocolHandler.onConnection != null) {
-      this.protocolHandler.onConnection(connection)
+    protected onConnection(connection: Connection) {
+        this._connections.unshift({
+            ...connection,
+            isClosed: false,
+        })
+        if (this.protocolHandler.onConnection != null) {
+            this.protocolHandler.onConnection(connection)
+        }
+        this.eventHandlers
+            .filter(x => x.event === 'connection')
+            .forEach(({ handler }) =>
+                handler({
+                    event: 'connection',
+                    id: connection.id,
+                }),
+            )
     }
-  }
 
-  public getConnections () {
-    return this._connections
-  }
+    public addEventListener(event: EventType, handler: EventHandler) {
+        this.eventHandlers.push({ event, handler })
+    }
 
-  refuseNewConnections = false
+    public removeEventListener(event: EventType, handler: EventHandler) {
+        this.eventHandlers = this.eventHandlers.filter(
+            x => !(x.event === event && x.handler === handler),
+        )
+    }
 
-  abstract get url (): Promise<string>
+    public getConnections() {
+        return this._connections
+    }
 
-  abstract dispose (): Promise<void>
+    refuseNewConnections = false
 
-  protected onClose (id: string) {
-    this._connections.forEach(x => {
-      if (x.id === id) {
-        x.isClosed = true
-      }
-    })
-  }
+    abstract get url(): Promise<string>
 
-  abstract disconnect (): void
+    abstract dispose(): Promise<void>
 
-  abstract start (): void
+    protected onClose(id: string) {
+        const listeners = this.eventHandlers.filter(x => x.event === 'disconnect')
+        this._connections.forEach(x => {
+            if (x.id === id) {
+                x.isClosed = true
+                listeners.forEach(({ handler }) =>
+                    handler({
+                        event: 'disconnect',
+                        id,
+                    }),
+                )
+            }
+        })
+    }
 
-  protected onMessage (connection: Connection, raw: string | Buffer) {
-    const message = this.protocolHandler.deserialize(raw)
-    this.protocolHandler.onMessage(connection, message)
-  }
+    abstract disconnect(): void
+
+    abstract start(): void
+
+    protected onMessage(connection: Connection, raw: string | Buffer) {
+        const message = this.protocolHandler.deserialize(raw)
+        this.protocolHandler.onMessage(connection, message)
+    }
 }
