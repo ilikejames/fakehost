2bbac62e874d692dc19ffc9ce3ed5ed09632d7c9
ilikejames <james.mountain@gmail.com>
2023-04-25 11:00:51 +0100

    build(rest): setup contract tests

--- packages/rest/test-fake-svc/src/router.ts
+++ packages/rest/test-fake-svc/src/router.ts
@@ -1,4 +1,10 @@
 import { createRouter, cors } from '@fakehost/fake-rest'
 import { userRoute } from './userRoute'
 
-export const router = createRouter().use(cors()).use('/user', userRoute)
+export const router = createRouter()
+    .use((req, res, next) => {
+        console.log('request', req)
+        next()
+    })
+    .use(cors())
+    .use('/user', userRoute)
