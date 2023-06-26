03d81517a7c8ad98220cd0624afdc1d2fd1b190a
ilikejames <james.mountain@gmail.com>
2023-06-26 15:47:14 +0100

    chore: ensure fakes services are wired in bundled mode

--- packages/test-web-app/src/fakeServices/signalr.ts
+++ packages/test-web-app/src/fakeServices/signalr.ts
@@ -1,15 +1,18 @@
-import { createInBrowserSignalr } from '@fakehost/signalr/browser'
-import { hubs } from '@fakehost/signalr-test-fake-svc'
 import { config } from '@/config'
 /**
  * Hijack the websocket for signalr and point to the fake service
  * embedded in the web app.
  */
-;(async function () {
+export const signalrReady = new Promise<void>(async resolve => {
     if (import.meta.env.VITE_BUNDLE_FAKES === 'true') {
+        const { createInBrowserSignalr } = await import('@fakehost/signalr/browser')
+        const { hubs } = await import('@fakehost/signalr-test-fake-svc')
         await createInBrowserSignalr<typeof hubs>({
             url: new URL(config.signalrUrl),
             hubs,
         })
+        resolve()
+    } else {
+        resolve()
     }
-})()
+})
